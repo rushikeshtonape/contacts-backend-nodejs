fa6ed0759c25b0121f22041f7db55f7489724289
RushikeshTonape <rushikeshtonape143@gmail.com>
2023-04-30 10:59:02 +0530

    Integrate JWT accessToken and  User Login

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
 //@des  register a user
@@ -36,10 +37,41 @@ const registerUser =asyncHandler( async(req,res)=>{
    
 })
 
+
+//@des  login a user
+//router post /api/users/login
+// Access Public
 const loginUser =asyncHandler( async(req,res)=>{
-    res.status(400).json({message:"Login the user"})
+    const {email, password} = req.body;
+    if(!email || !password){
+        res.status(400)
+        throw new Error('all fields are mandantory')
+    }
+    const user = await User.findOne({email})
+    if(user && (await bcrypt.compare(password , user.password))){
+
+        const accessToken = jwt.sign(
+            {
+            user: {
+                username: user.username,
+                email: user.email,
+                id:user.id
+            }
+             },process.env.ACCESS_TOKEN_SECRET,
+             {expiresIn: "1m"}
+        );
+        res.status(200).json({accessToken})
+    }else{
+        res.status(401)
+        throw new Error("email or password is not valid");
+    }
+   
 });
 
+
+//@des   current user
+//router get /api/users/current
+// Access Private
 const currentUser =asyncHandler( async(req,res)=>{
     res.status(400).json({message:"current user"})
 });
